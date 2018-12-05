a53f3fd680a58a46e979b935723e28ae04157f5b
erethizon <barthelmess@stlawu.edu>
2018-12-05 17:32:01 -0500

    initialize git repo for zooniverse work

---
+++ DataProcessing-master/projects/in-development/project-specs.R
@@ -0,0 +1,74 @@
+
+### MUST SPECIFY FIELDS SO FLATTENING WILL WORK. Examples included for Michigan Zoomin, Colorado Corridors, Wildcam Darien
+
+
+## If you have single or multiple choice questions and do not specify them, the flattener will ignore them. 
+## If you do not specify column out values, the flattener will use column in values.
+# 
+# REQUIRED FIELDS
+
+# # Michigan
+# project_name <- "michigan-zoomin"
+# classifications_file <- "data/michigan-zoomin-classifications.csv"
+# survey_id <- c("T3")
+# workflow_id_num <- 2276
+# workflow_version_num <- 463.55
+# single_choice_Qs <-  c("HOWMANYANIMALSDOYOUSEE")
+# single_choice_colnames  <-  c("how_many")
+# multi_choice_Qs <- c("WHATISTHEANIMALSDOING")
+# multi_choice_colnames <- c("behavior")
+
+
+
+# # Colorado Corridors
+# project_name <- "colorado-corridors"
+# classifications_file <- "data/colorado-corridors-project-classifications.csv"
+# survey_id <- c("T0")
+# workflow_id_num <- 1187
+# workflow_version_num <- 1158.155
+# single_choice_Qs <- c("HOWMANYANIMALSDOYOUSEE")
+
+
+# # Camera Catalogue
+# project_name <- "camera-catalogue"
+# classifications_file <- "projects/sample_data/camcat_sa.csv"
+# survey_id <- "T0"
+# workflow_id_num <- 2672
+# workflow_version_num <- 325.7
+# single_choice_Qs <- "WHCHSDFTHNMLSVSBL"
+# single_choice_colnames <- "which_side"
+
+# Elephant expeditions
+# project_name <- "elephant"
+# classifications_file <- "data/elephant-expedition-classifications.csv"
+# workflow_id_num <- 3559
+# workflow_version_num <- 30.30
+# survey_id <- c("T0")
+# single_choice_Qs <-  c("HOWMANY")
+# single_choice_colnames  <-  c("how_many")
+
+
+# # Chicago
+# project_name <- "chicago"
+# classifications_file <- "data/chicago-wildlife-watch-classifications.csv"
+# workflow_id_num <- 4377
+# workflow_version_num <- 2.20
+# 
+# survey_id <- c("T0")
+# single_choice_Qs <-  c("HOWMANY")
+# single_choice_colnames  <-  c("how_many")
+# question_task_id <- c("T1")
+
+# Chicago also needs to deal wth these versions too, which may or may not have additional questions
+# workflow_id_num <- 2334
+# workflow_version_num <- c(397.41, 406.45)
+# workflow_id_num <- 3054
+# workflow_version_num <- 5.70
+
+
+
+
+
+
+
+
