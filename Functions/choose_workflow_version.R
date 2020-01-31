adbff0ad01f5ce2ead6da70f6cf90cbc55d783c8
Erika Barthelmess <barthelmess@stlawu.edu>
2020-01-31 13:36:29 -0500

    add functions folder with two functions

---
+++ Functions/choose_workflow_version.R
@@ -0,0 +1,39 @@
+##R function to prompt user to select proper workflow and workflow version 
+choose_my_workflow<-function(df){
+     #turn on dplyr
+     require(dplyr)
+     
+     select_workflow<- function(df) {
+          #identify workflows by making workflow_id             #into a factor
+          df$workflow_id<-as.factor(df$workflow_id)
+          workflows<-levels(df$workflow_id) 
+          return(workflows)
+          #works
+     }
+
+     workflows<- select_workflow(df)
+
+     #now ask user which workflow to use
+
+     my_selection<- menu(workflows, 
+     title= "Which workflow do you want?")
+      
+     my_selection_filtered<-workflows[my_selection]
+     #now find the desired version of the workflow
+      DF2<-filter(df, workflow_id == my_selection_filtered)
+      DF2$workflow_version<-as.factor(
+           DF2$workflow_version)
+      
+      versions<-levels(DF2$workflow_version)
+
+      my_wkflow_version<- menu(
+          versions, 
+          title = "Which workflow version do you want?")
+      #now filter df to that workflow version
+      
+      DF3<-filter(
+           DF2, 
+           workflow_version == versions[my_wkflow_version])
+}
+
+#this works, but will require that the user know which workflow and workflow version they desire.
